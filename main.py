3e1bf04cf4928cb907387b5ddff6be3704cf81af
Hamza Ahmed Qureshi <qureshih@student.unimelb.edu.au>
2020-12-09 04:02:14 +1100

    Stores Multiple Historical Stock Price

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
-from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import requests
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
+
 
 DRIVER_LOC = "C:\\Program Files (x86)\\msedgedriver.exe"
 
 
 def main():
-    driver = webdriver.Edge(DRIVER_LOC)
-    driver.get("https://au.finance.yahoo.com/quote/AAPL?p=AAPL")
+    index_codes = []
 
-    # code = get_code()
-    # navigate_to_stock(driver, code)
+    stock_code = get_code()[0]
+    index_codes.extend(get_code())
+
+    data = pd.DataFrame.from_dict(get_data(stock_code), orient="index",
+                                  columns=[stock_code.upper()])
+    data = data.rename_axis("Date")
+    for code in index_codes:
+        data[code.upper()] = get_data(code).values()
+
+    print(data.to_string())
+    input()
+
+
+def get_data(stock_code):
+
+    driver = webdriver.Edge(DRIVER_LOC)
+    driver.get("https://au.finance.yahoo.com/")
+
+    navigate_to_stock(driver, stock_code)
 
     start_date = set_start_date(driver)
-    # scroll_to_date(driver, start_date)
-    stock_prices = get_data(driver)
-    for key, value in stock_prices.items():
-        print(key, value)
-    input()
-    driver.quit()
+
+    if not start_date:
+        return False
+
+    scroll_to_date(driver, start_date)
+    return read_data(driver)
 
 
 def navigate_to_stock(driver, code):
@@ -41,7 +58,7 @@ def set_start_date(driver):
         driver_wait = WebDriverWait(driver, 10)
 
         driver_wait.until(EC.presence_of_element_located(
-            (By.XPATH, "//li[@data-test='HISTORICAL_DATA']"))).click()
+                    (By.XPATH, "//span[text()='Historical data']"))).click()
         driver_wait.until(EC.presence_of_element_located(
             (By.XPATH, "//table[@data-test='historical-prices']")))
         driver_wait.until(EC.presence_of_element_located(
@@ -53,6 +70,7 @@ def set_start_date(driver):
         element = driver_wait.until(EC.presence_of_element_located(
             (By.XPATH, "//div[@class='Pos(r) D(ib) Va(m) Mstart(8px)']")))
         start_date = re.search("(.*) - .*", element.text).group(1)
+
         return start_date
 
     except TimeoutException:
@@ -71,26 +89,23 @@ def scroll_to_date(driver, start_date):
             element.send_keys(Keys.END)
 
 
-def get_data(driver):
+def read_data(driver):
 
     price_dict = dict()
     table = driver.find_element_by_xpath("//table[@data-test='historical-prices']")
+
     for row in table.find_elements_by_tag_name("tr"):
         elements = row.find_elements_by_tag_name("td")
         if len(elements) < 6:
             continue
-        price_dict[elements[0].text] = elements[5].text
+        price_dict[elements[0].text] = float(elements[5].text.replace(",", ""))
 
+    driver.quit()
     return price_dict
 
 
-def get_soup(url):
-    return BeautifulSoup(requests.get(url).text, "html.parser")
-
-
 def get_code():
-    return "aapl"
-    return input("Enter Desired Stock Code")
+    return input("Enter Desired Stock Code: ").split(" ")
 
 
 if __name__ == '__main__':
